16b2483fb347f6b1ebc78ff33c757313dc9066df
Carl Alexander <carlalexander@gmail.com>
2013-11-20 12:11:18 -0500

    Updated to use official nginx repo

--- manifests/package.pp
+++ manifests/package.pp
@@ -14,7 +14,23 @@
 #
 # This class file is not called directly
 class nginx::package {
+  Exec {
+    path => '/bin:/sbin:/usr/bin:/usr/sbin',
+  }
+
+  exec { 'nginx_signing.key':
+    command => 'wget -O- http://nginx.org/keys/nginx_signing.key | apt-key add -',
+    unless  => 'apt-key list | grep -c nginx_signing',
+  }
+
+  exec { 'nginx.list':
+    command => "echo 'deb http://nginx.org/packages/ubuntu/ quantal nginx' >> /etc/apt/sources.list.d/nginx.list && apt-get update",
+    creates => '/etc/apt/sources.list.d/nginx.list',
+    require => Exec['nginx_signing.key']
+  }
+
   package { 'nginx':
     ensure => latest,
+    require => Exec['nginx.list'],
   }
 }
